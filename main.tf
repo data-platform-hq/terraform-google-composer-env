a02006b62d40509c9a5156acc6eb23ec15a449e1
Ivan Smahlenko <ivan_smahlenko@epam.com>
2023-01-04 01:05:50 +0200

    fix: removed empty lines in main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 resource "google_composer_environment" "main" {
   name = "${var.env}-${var.product_base_name}-composer"
   config {
-
     software_config {
       image_version = var.image_version
     }
-
     environment_size = var.environment_size
-
     node_config {
       network         = var.network
       subnetwork      = var.subnet
